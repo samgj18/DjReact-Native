ef11bafb031628424dff06ded2a293b4adcee6f0
samgj18 <samgj_18@hotmail.com>
2019-04-22 08:34:48 -0500

    Activity picker added

--- Applications/BleModule/Ble.js
+++ Applications/BleModule/Ble.js
@@ -8,7 +8,8 @@ import {
   NativeModules,
   ListView,
   Platform,
-  PermissionsAndroid
+  PermissionsAndroid,
+  Picker
 } from 'react-native'
 import AsyncStorage from '@react-native-community/async-storage'
 import NetInfo from "@react-native-community/netinfo";
@@ -17,8 +18,12 @@ import { Icon } from 'react-native-elements'
 import Toast, { DURATION } from 'react-native-easy-toast'
 import BleManager from 'react-native-ble-manager'
 
-const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
-const BleManagerModule = NativeModules.BleManager;
+
+
+const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 })
+const BleManagerModule = NativeModules.BleManager
+
+
 
 /*
 Sometimes an app needs to access a platform API and React Native doesn't have a corresponding module yet. 
@@ -26,7 +31,7 @@ Maybe you want to reuse some existing Objective-C, Swift or C++ code without hav
 or write some high performance. NativeModules will not expose any methods of BleManager to JavaScript unless explicitly
 told to, as we will further.
 */
-const BleManagerEmitter = new NativeEventEmitter(BleManagerModule);
+const BleManagerEmitter = new NativeEventEmitter(BleManagerModule)
 
 /* Subscribing events from BleManagerModule by creating a new NativeEventEmitter instance around this module, this
 will be helpful later the in code */
@@ -36,7 +41,7 @@ class Ble extends Component {
     super(props)
 
     const { navigation } = this.props;
-    const userID = navigation.getParam('userID');
+    const userID = navigation.getParam('userID')
     this.state = {
       scanning: false,
       peripherals: new Map(),
@@ -46,6 +51,7 @@ class Ble extends Component {
         coilOneData: '',
         coilTwoData: '',
       }],
+      pickerValue: '',
       status: '',
       dataDoubleVoltage: '',
       dataDoubleVoltageCoilOne: '',
@@ -55,71 +61,71 @@ class Ble extends Component {
     }
     this.dataFlag = false
     this.datRows = []
-    this.handleDiscoverPeripheral = this.handleDiscoverPeripheral.bind(this);
-    this.handleDisconnectedPeripheral = this.handleDisconnectedPeripheral.bind(this);
+    this.handleDiscoverPeripheral = this.handleDiscoverPeripheral.bind(this)
+    this.handleDisconnectedPeripheral = this.handleDisconnectedPeripheral.bind(this)
   }
 
 
 
   componentDidMount() {
-    this.handlerDiscover = BleManagerEmitter.addListener('BleManagerDiscoverPeripheral', this.handleDiscoverPeripheral);
-    this.handlerDisconnect = BleManagerEmitter.addListener('BleManagerDisconnectPeripheral', this.handleDisconnectedPeripheral);
+    this.handlerDiscover = BleManagerEmitter.addListener('BleManagerDiscoverPeripheral', this.handleDiscoverPeripheral)
+    this.handlerDisconnect = BleManagerEmitter.addListener('BleManagerDisconnectPeripheral', this.handleDisconnectedPeripheral)
     /* This code line handles the discover of new devices, the BleManagerEmitter (NativeEventEmitter), as soon as the 
     component is render (componentDidMount) it will keep doing the BleManager function "BleManagerDiscoverPeripheral",
     which will notificate or create an event everytime the scan finds a new peripheral */
-    NetInfo.isConnected.addEventListener('connectionChange', this.handleConnectionChange);
+    NetInfo.isConnected.addEventListener('connectionChange', this.handleConnectionChange)
     NetInfo.isConnected.fetch().done(
-      (isConnected) => { this.setState({ status: isConnected }); }
+      (isConnected) => { this.setState({ status: isConnected }) }
     );
     /*NetInfo informs about the network status whether if it's connected or not */
     if (Platform.OS === 'android' && Platform.Version >= 23) {
       PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION).then((result) => {
         if (result) {
-          console.log("All permissions given");
+          console.log("All permissions given")
         } else {
           PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION).then((result) => {
             if (result) {
-              console.log("User accept");
+              console.log("User accept")
             } else {
-              console.log("User refuse");
+              console.log("User refuse")
             }
-          });
+          })
         }
-      });
+      })
     }
 
     /* In case of not having the permission to access to location (in Android Devices) this will ask the user for permission
     to access for BLE working properly */
     BleManager.enableBluetooth()
       .then(() => {
-        this.refs.toast.show('Bluetooh activado', DURATION.LENGTH_LONG);
+        this.refs.toast.show('Bluetooh activado', DURATION.LENGTH_LONG)
       })
       .catch((error) => {
-        this.refs.toast.show('Active el bluetooh' + error, DURATION.LENGTH_LONG);
-      });
+        this.refs.toast.show('Active el bluetooh' + error, DURATION.LENGTH_LONG)
+      })
     /* If the BT is not enable, this will ask the user to enable it right after the component is render */
     BleManager.start({ showAlert: false })
       .then(() => {
         this.refs.toast.show('Módulo BT inicializado', DURATION.LENGTH_LONG);
-        BleManager.scan([], 120);
-      });
+        BleManager.scan([], 120)
+      })
     /*Start scanning for BT devices for a time period of 120 seconds*/
   }
 
   componentWillUnmount() {
-    this.handlerDiscover.remove();
-    this.handlerDisconnect.remove();
-    NetInfo.isConnected.removeEventListener('connectionChange', this.handleConnectionChange);
+    this.handlerDiscover.remove()
+    this.handlerDisconnect.remove()
+    NetInfo.isConnected.removeEventListener('connectionChange', this.handleConnectionChange)
   }
 
   handleConnectionChange = (isConnected) => {
-    this.setState({ status: isConnected });
+    this.setState({ status: isConnected })
   }
 
   handleDiscoverPeripheral(peripheral) {
-    var peripherals = this.state.peripherals;
+    var peripherals = this.state.peripherals
     if (!peripherals.has(peripheral.id)) {
-      peripherals.set(peripheral.id, peripheral);
+      peripherals.set(peripheral.id, peripheral)
       this.setState({ peripherals })
     }
   }
@@ -130,9 +136,9 @@ class Ble extends Component {
   */
 
   scanForPeriodOfTime = () => {
-    this.dataFlag = true
+    this.dataFlag = this.dataFlag ? this.dataFlag = false : this.dataFlag = true
     this.setState({
-      iconColor: 'green'
+      iconColor: this.dataFlag ? iconColor = 'green' : iconColor = 'red'
     })
     console.log(this.dataFlag + this.state.iconColor)
     if (this.dataFlag) {
@@ -162,7 +168,7 @@ class Ble extends Component {
   to false managing to disconnect the device
   */
 
-  sendDataToServer = async (value, coilOne, coilTwo, id) => {
+  sendDataToServer = async (value, coilOne, coilTwo, id,datetime,activity) => {
     try {
       let config = {
         method: 'POST',
@@ -175,7 +181,8 @@ class Ble extends Component {
           voltage_coil_1: coilOne,
           voltage_coil_2: coilTwo,
           voltage_generated_by_user: value,
-
+          activity,
+          datetime
         })
       }
       const URL = 'http://72.14.177.247/api/data/'
@@ -190,6 +197,9 @@ class Ble extends Component {
   /*
     Sending data to the server
   */
+ 
+  //alert('A list was submitted: ' + this.state.formvalue);
+
 
   async removeItemValue() {
     try {
@@ -241,7 +251,7 @@ class Ble extends Component {
                     coilOneData = res[0]
                     coilTwoData = res[1]
                     userVoltageData = res[2]
-                    console.log( this.props.token)
+                    console.log(this.props.token)
                     this.setState({
                       dataDoubleVoltage: userVoltageData,
                       dataDoubleVoltageCoilOne: coilOneData,
@@ -274,7 +284,7 @@ class Ble extends Component {
                                 return row[header]
                               })
                               this.datRows.push(values)
-                              this.sendDataToServer(row.voltage, row.coilOneData, row.coilTwoData, row.id)
+                              this.sendDataToServer(row.voltage, row.coilOneData, row.coilTwoData, row.id,null,this.state.pickerValue)
                             }
                             this.removeItemValue()
                             NetInfo.isConnected.addEventListener('connectionChange', this.handleConnectionChange);
@@ -283,7 +293,7 @@ class Ble extends Component {
                             )
                             console.log('Base de datos local no vacía enviando todos los datos')
                           } else {
-                            this.sendDataToServer(this.state.dataDoubleVoltage, this.state.dataDoubleVoltageCoilOne, this.state.dataDoubleVoltageCoilTwo, this.state.userID)
+                            this.sendDataToServer(this.state.dataDoubleVoltage, this.state.dataDoubleVoltageCoilOne, this.state.dataDoubleVoltageCoilTwo, this.state.userID,null,this.state.pickerValue)
                             this.removeItemValue()
                             console.log('Base de datos local vacía enviando dato')
                             NetInfo.isConnected.addEventListener('connectionChange', this.handleConnectionChange)
@@ -318,7 +328,7 @@ class Ble extends Component {
                     } catch (error) {
                       this.refs.toast.show(error, DURATION.LENGTH_LONG);
                     }
-                  }) 
+                  })
                 })
                 .catch((error) => {
                   console.log('Connection error', error);
@@ -377,7 +387,18 @@ class Ble extends Component {
             color={this.state.iconColor}
             onPress={this.scanForPeriodOfTime.bind(this)} />
         </View>
-
+        <View style={styles.boxTwo}>
+          <Picker
+            selectedValue={this.state.pickerValue}
+            onValueChange={(itemValue, itemIndex) => this.setState({ pickerValue: itemValue })}
+          >
+            <Picker.Item label='Seleccione una actividad' value='' />
+            <Picker.Item label='Caminar' value='1' />
+            <Picker.Item label='Saltar' value='2' />
+            <Picker.Item label='Correr' value='3' />
+            <Picker.Item label='Subir o Bajar escaleras' value='4' />
+          </Picker>
+        </View>
         <ListView
           enableEmptySections={true}
           dataSource={dataSource}
@@ -418,7 +439,10 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 11
+    padding: 11,
+  },
+  boxTwo: {
+    width: '100%',
   },
   row: {
     margin: 10
@@ -432,3 +456,5 @@ const mapStateToProps = state => {
 }
 
 export default connect(mapStateToProps, null)(Ble)
+
+
